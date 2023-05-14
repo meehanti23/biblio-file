9bcea6289c7b156f1e251ea7b8e409f0a6650f54
Tim Meehan <meehanti23@gmail.com>
2023-05-14 19:46:39 -0400

    Some homepage styling

---
+++ client/src/components/Homepage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Homepage = (props) => {
+    return (
+        <div className='primary home-box'>
+            <h1>Catalog Your Personal Library!</h1>
+            <h3>Created by Tim Meehan</h3>
+        </div>
+    )
+}
+
+export default Homepage;
