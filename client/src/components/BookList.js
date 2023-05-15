d1e78d930beb3b2c04f6b43e4605c99abac02fdc
Tim Meehan <meehanti23@gmail.com>
2023-05-14 21:27:59 -0400

    Initial Book List rendered to front end

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,9 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import BookTile from "./BookTile";
 
 const BookList = (props) => {
+    const [books, setBooks] = useState([]);
+
+    const getBooks = async () => {
+        try {
+            const response = await fetch("/api/v1/books");
+            if (!response.ok) {
+                const errorMessage = `${response.status} (${response.statusText})`;
+                const error = new Error(errorMessage);
+                throw error;
+            }
+            const data = await response.json();
+            setBooks(data.books);
+        } catch (error) {
+            console.error(`Error in fetch: ${error.message}`);
+        }
+    };
+
+    useEffect(() => {
+        getBooks();
+    }, []);
+
+    const mappedBooks = books.map((book) => {
+        return (
+            <BookTile
+                key={book.id}
+                id={book.id}
+                title={book.title}
+                author={book.author}
+            />
+        );
+    });
+
     return (
-        <div className='primary home-box'>
-            <h1>Book Shelf</h1>
+        <div className='primary home-box grid-x'>
+            <h1 className="cell">Book Shelf</h1>
+            {mappedBooks}
         </div>
     )
 }
