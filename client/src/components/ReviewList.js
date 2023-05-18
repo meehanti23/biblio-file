014f5721c981206b565116f2dc02b7e7b232fa8d
Tim Meehan <meehanti23@gmail.com>
2023-05-18 12:07:52 -0400

    remove debugger

--- client/src/components/ReviewList.js
+++ client/src/components/ReviewList.js
@@ -2,7 +2,6 @@ import React from "react";
 
 const ReviewList = (props) => {
     const reviews = props.reviews.map((review) => {
-        debugger
         return (
             <div key={review.id}>
                 <p>{review.reviewBody}</p>
