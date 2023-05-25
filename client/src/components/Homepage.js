d69664a2e55651cc321df222525fda3489b79ae4
Tim Meehan <meehanti23@gmail.com>
2023-05-25 12:50:48 -0400

    Added filters

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -6,9 +6,9 @@ const Homepage = (props) => {
         <div className='primary home-box'>
             <h1 className='page-title'>Catalog Your Personal Library!</h1>
             <div className='about-link-wrapper'>
-                <Link to="/about" className="about-link">About the Developer</Link>
             </div>
             <img src="https://personal-library-bucket.s3.amazonaws.com/pngwing.com.png" alt="homepage-shelf"/>
+                <Link to="/about" className="about-link">About the Developer</Link>
         </div>
     )
 }
