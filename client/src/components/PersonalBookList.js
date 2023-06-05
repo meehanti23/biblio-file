df8f1eaf5d10600a8d151c2624a58c2b4d6edaab
Tim Meehan <meehanti23@gmail.com>
2023-06-05 09:05:21 -0400

    Small changes to PersonalBookList.js

--- client/src/components/PersonalBookList.js
+++ client/src/components/PersonalBookList.js
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import PersonalBookTile from './PersonalBookTile';
-import axios from 'axios';
-import translateServerErrors from '../services/translateServerErrors';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBookOpen, faChartPie, faChartColumn } from '@fortawesome/free-solid-svg-icons';
 import GenrePieChart from './dataVisualization/GenrePieChart';
