32cb19ff13e361b20d5899cf4ee5411a9726671b
Tim Meehan <meehanti23@gmail.com>
2023-06-12 09:42:21 -0400

    About developer update

--- client/src/components/AboutDeveloper.js
+++ client/src/components/AboutDeveloper.js
@@ -8,6 +8,7 @@ const AboutDeveloper = () => {
             <p className="about-tim">Tim is a passionate developer with a fresh perspective and a knack for coding. With a background in hospitality, he can bring a unique blend of creativity and problem-solving skills to his projects.</p>
             <p className="about-tim">Specializing in React and Express, Tim finds joy in building intuitive and interactive web applications. Data visualization is his playground, where he transforms complex information into captivating visuals. Tim thrives on learning and constantly seeks new challenges to further hone his coding prowess.</p>
             <ul className='homepage-list'><b>Developed With Help From:</b>
+                <li>Google Maps -- Geolocation, Places, React Google Maps</li>
                 <li>Google Book API</li>
                 <li>Open Trivia Database</li>
                 <li>Goodreads</li>
